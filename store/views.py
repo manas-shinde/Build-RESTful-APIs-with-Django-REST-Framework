649339f69bf54cf984999297e042b38a4368f6c4
manas-shinde <manas.a.shinde11@gmail.com>
2023-03-06 13:25:38 +0530

    fix : Now returning the correct product using product_id.

--- store/views.py
+++ store/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .models import Product
+from .serializers import ProductSerializer
 # Create your views here.
+
 
 @api_view()
 def product_list(request):
     return Response('OKAY !!!')
 
+
 @api_view()
-def product_detail(request,id):
-    return Response("OK")
+def product_detail(request, id):
+    # return Response("ok")
+    product = Product.objects.get(pk=int(id))
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
