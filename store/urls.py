6ed01c125387624f16d7b2a16a62a941a7a40c04
manas-shinde <manas.a.shinde11@gmail.com>
2023-03-07 09:56:14 +0530

    feat : New API endpoint for collections

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('products/', views.product_list),
-    path('products/<int:id>/', views.product_detail)
+    path('products/<int:id>/', views.product_detail),
+    path('collections/<int:pk>', views.collection_details,
+         name='collection-detail')
 ]
