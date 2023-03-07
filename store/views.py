84a8878b295ebec59654fabef0fd65a87f267eaf
manas-shinde <manas.a.shinde11@gmail.com>
2023-03-07 09:58:44 +0530

    feat : New view for collection details
    to read collections object from database and return.

--- store/views.py
+++ store/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import get_list_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Collection
+from .serializers import ProductSerializer, CollectionSerializer
 # Create your views here.
 
 
 @api_view()
 def product_list(request):
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True)
+    queryset = Product.objects.select_related('collection').all()
+    serializer = ProductSerializer(
+        queryset, many=True, context={'request': request})
     return Response(serializer.data)
 
 
@@ -18,4 +19,13 @@ def product_list(request):
 def product_detail(request, id):
     product = get_list_or_404(Product, pk=int(id))
     serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view()
+def collection_details(request, pk):
+    # return Response("OK")
+    collections = get_list_or_404(Collection, pk=int(pk))
+    serializer = CollectionSerializer(collections)
+    print(serializer)
     return Response(serializer.data)
