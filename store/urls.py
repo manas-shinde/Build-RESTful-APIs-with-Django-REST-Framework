895b6447c8a20664679a2a7d80c3fff2bd5ebede
manas-shinde <manas.a.shinde11@gmail.com>
2023-03-10 11:13:50 +0530

    feat : ModelViews for collection and products
    Now, we don't need to write separate class for list and specific object for operations.

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include
 import debug_toolbar
+from rest_framework.routers import SimpleRouter, DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register('products', viewset=views.ProductViewSet)
+router.register('collections', viewset=views.CollectionViewSet)
 
 urlpatterns = [
-    path('products/', views.ProductsList.as_view()),
-    path('products/<int:pk>/', views.ProductDetails.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('collections/<int:pk>', views.CollectionDetails.as_view())
+    path('', include(router.urls))
+    # path('products/', views.ProductsList.as_view()),
+    # path('products/<int:pk>/', views.ProductDetails.as_view()),
+    # path('collections/', views.CollectionList.as_view()),
+    # path('collections/<int:pk>', views.CollectionDetails.as_view())
 ]
--- store/views.py
+++ store/views.py
@@ -3,11 +3,45 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
 
-from .models import Product, Collection
+from .models import Product, Collection, OrderItem
 from .serializers import ProductSerializer, CollectionSerializer
+
+# ModelViewset
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+
+    serializer_class = ProductSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+    def destroy(self, request, *args, **kwargs):
+
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
+            return Response({'can not delete this product becase order item are related to it.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        return super().destroy(request, *args, **kwargs)
+        # This destory() is same logic that we've writen in delete() then only difference is the if condition that we have before deleteing product . so that why we just move condition above and just call destory() to do deleteion.
+
+
+class CollectionViewSet(ModelViewSet):
+    queryset = Collection.objects.annotate(
+        products_count=Count('products')).all()
+
+    serializer_class = CollectionSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        collection = get_object_or_404(Collection, pk=kwargs["pk"])
+        if collection.products.count() > 0:
+            return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        return super().destroy(request, *args, **kwargs)
 
 
 # Belowed are Generie-based view
