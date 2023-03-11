76bb8d6533b46123bf44beb14258bd441b73fc54
manas-shinde <manas.a.shinde11@gmail.com>
2023-03-11 12:39:08 +0530

    feat : filter for text based search

--- store/views.py
+++ store/views.py
@@ -7,6 +7,7 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.filters import SearchFilter
 
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -31,10 +32,14 @@ class ReviewViewSet(ModelViewSet):
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
+
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+
     filterset_class = ProductFilter
 
+    search_fields = ['title', 'description']
     # def get_queryset(self):
     #     """Customize filter for product list based on collection id
 
