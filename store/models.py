af128c4c0914e1ed504bded02636eefb5a1b65d2
manas-shinde <manas.a.shinde11@gmail.com>
2023-03-13 23:46:46 +0530

    feat : creating new custom user model as per requirement of project. also creating profile of user.

--- store/models.py
+++ store/models.py
@@ -1,7 +1,10 @@
 from django.core.validators import MinValueValidator
+from django.contrib import admin
 from django.db import models
 
 from uuid import uuid4
+
+from storefront import settings
 
 
 class Promotion(models.Model):
@@ -59,12 +62,22 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True, blank=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.user.first_name} {self.user.last_name}'
+
+    @admin.display(ordering="user__first_name")
+    def first_name(self):
+        return self.user.first_name
+
+    @admin.display(ordering="user__last_name")
+    def last_name(self):
+        return self.user.last_name
 
     class Meta:
-        ordering = ['first_name', 'last_name']
+        ordering = ['user__first_name', 'user__last_name']
 
 
 class Order(models.Model):
