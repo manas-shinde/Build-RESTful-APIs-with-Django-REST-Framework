c633774cb672c535f5ddabe0b8b51ece6978230c
manas-shinde <manas.a.shinde11@gmail.com>
2023-03-16 23:10:10 +0530

    feat : custom permission define.

--- storefront/settings.py
+++ storefront/settings.py
@@ -151,9 +151,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
 }
 
 AUTH_USER_MODEL = 'core.User'
