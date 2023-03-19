3e897e6091c548debaa1a264b3eb3bbf50efa0f9
manas-shinde <manas.a.shinde11@gmail.com>
2023-03-19 19:58:32 +0530

    feat : Saving the order item and implemented with the transction.

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,6 @@
-from rest_framework import serializers
 from decimal import Decimal
-
+from django.db import transaction
+from rest_framework import serializers
 from .models import Order, OrderItem, Product, Collection, Review, Cart, CartItem, Customer
 
 
@@ -152,10 +152,26 @@ class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
     def save(self, **kwargs):
-        print(self.validated_data['cart_id'])
-        print(self.context['user_id'])
+        with transaction.atomic():
+            # Performating these saving and updating database values with transction object means either all operations will perform or not
+            cart_id = self.validated_data['cart_id']
+            user_id = self.context['user_id']
 
-        (customer, is_customer_created) = Customer.objects.get_or_create(
-            user_id=self.context['user_id'])
+            (customer, is_customer_created) = Customer.objects.get_or_create(
+                user_id=self.context['user_id'])
 
-        Order.objects.create(customer=customer)
+            order = Order.objects.create(customer=customer)
+            # TODO : save the order item
+
+            cart_items = CartItem.objects.select_related(
+                'product').filter(cart_id=cart_id)
+            order_items = [
+                OrderItem(
+                    order=order,
+                    product=item.product,
+                    unit_price=item.product.unit_price,
+                    quantity=item.quantity
+                ) for item in cart_items
+            ]
+            OrderItem.objects.bulk_create(order_items)
+            Cart.objects.filter(pk=cart_id).delete()
