9f7c2cd72dda27f333f0389515deea618ad30d24
manas-shinde <manas.a.shinde11@gmail.com>
2023-03-22 16:43:56 +0530

    feat (Added fixture) : Fixture for createing APIClient object.

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,48 +1,42 @@
 from django.contrib.auth.models import User
-from rest_framework.test import APIClient
 from rest_framework import status
 import pytest
 
 
 @pytest.mark.django_db
 class TestCreateCollections:
-    def test_if_user_is_anonymous_returns_401(self):
+    def test_if_user_is_anonymous_returns_401(self, api_client):
         # AAA (Arrange , Act , Assert)
-
         # Arrange
 
         # Act
-        client = APIClient()
-        response = client.post('/store/collections/', {'title': 'a'})
+        response = api_client.post('/store/collections/', {'title': 'a'})
 
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_if_user_is_not_admin_returns_403(self):
+    def test_if_user_is_not_admin_returns_403(self, api_client):
         # Act
-        client = APIClient()
-        client.force_authenticate(user={})
-        response = client.post('/store/collections/', {'title': 'a'})
+        api_client.force_authenticate(user={})
+        response = api_client.post('/store/collections/', {'title': 'a'})
 
         # Assert
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_if_data_is_invalid_returns_400(self):
+    def test_if_data_is_invalid_returns_400(self, api_client):
         # Act
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
-        response = client.post('/store/collections/', {'title': ''})
+        api_client.force_authenticate(user=User(is_staff=True))
+        response = api_client.post('/store/collections/', {'title': ''})
 
         # Assert
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['title'] is not None
 
-    def test_if_data_is_valid_returns_200(self):
+    def test_if_data_is_valid_returns_200(self, api_client):
         # Act
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
-        response = client.post('/store/collections/',
-                               {'title': 'Beauty Products'})
+        api_client.force_authenticate(user=User(is_staff=True))
+        response = api_client.post('/store/collections/',
+                                   {'title': 'Beauty Products'})
 
         # Assert
         assert response.status_code == status.HTTP_201_CREATED
