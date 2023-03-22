3da2d98f0f1015992c956f607fc12098538379f1
manas-shinde <manas.a.shinde11@gmail.com>
2023-03-22 17:06:15 +0530

    feat (fixture authenticate_user) : Created one more fixture for authenticate_user and implemented in test cases.

--- store/tests/conftest.py
+++ store/tests/conftest.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 import pytest
 
@@ -5,3 +6,10 @@ import pytest
 @pytest.fixture
 def api_client():
     return APIClient()
+
+
+@pytest.fixture
+def authenticate_user(api_client):
+    def do_authenticate_user(is_staff=False):
+        api_client.force_authenticate(user=User(is_staff=is_staff))
+    return do_authenticate_user
