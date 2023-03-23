fa99afd37fc56d0f721071fecf852d6cfca8933a
manas-shinde <manas.a.shinde11@gmail.com>
2023-03-23 16:28:30 +0530

    fix : Added low level cacheing for hello endpint.

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,14 @@
+from django.core.cache import cache
 from django.shortcuts import render
 import requests
 
 
 def say_hello(request):
-    requests.get('https://httpbin.org/delay/3')
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    key = 'httpbin_result'
+    # Low-level cache API
+    if cache.get(key) is None:
+        response = requests.get('https://httpbin.org/delay/3')
+        data = response.json()
+        cache.set(key, data)
+
+    return render(request, 'hello.html', {'name': cache.get(key)})
