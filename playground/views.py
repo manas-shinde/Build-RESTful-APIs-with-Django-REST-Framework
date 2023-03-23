98678a18f0743afe609550b7618b24f6dd38cbf2
manas-shinde <manas.a.shinde11@gmail.com>
2023-03-23 16:09:21 +0530

    feat : setup the slow API to use cache.

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+import requests
 
 
 def say_hello(request):
+    requests.get('https://httpbin.org/delay/3')
     return render(request, 'hello.html', {'name': 'Mosh'})
