ff50fed0b8792ba535c1e875acf4db858036c390
manas-shinde <manas.a.shinde11@gmail.com>
2023-03-23 16:27:56 +0530

    fix (CACHES config): added global timeout for cacheing.

--- storefront/settings.py
+++ storefront/settings.py
@@ -181,6 +181,7 @@ CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379/2",
+        "timeout": 10*60,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
